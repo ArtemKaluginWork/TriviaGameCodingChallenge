581c4d77be3f1d00b85d33afc16269c7dd837d7e
Artem Kalugin <a.kalugin@dunice.net>
2019-02-20 09:21:18 +0300

    remove unused package

--- src/helpers/decodeHtml.js
+++ src/helpers/decodeHtml.js
@@ -1,11 +1,12 @@
 export default function decodeHTMLEntities(str) {
+  let newStr = str
   const element = document.createElement('div');
-  if (str && typeof str === 'string') {
-    str = str.replace(/<script[^>]*>([\S\s]*?)<\/script>/gim, '');
-    str = str.replace(/<\/?\w(?:[^"'>]|"[^"]*"|'[^']*')*>/gim, '');
-    element.innerHTML = str;
-    str = element.textContent;
+  if (newStr && typeof newStr === 'string') {
+    newStr = newStr.replace(/<script[^>]*>([\S\s]*?)<\/script>/gim, '');
+    newStr = newStr.replace(/<\/?\w(?:[^"'>]|"[^"]*"|'[^']*')*>/gim, '');
+    element.innerHTML = newStr;
+    newStr = element.textContent;
     element.textContent = '';
   }
-  return str;
+  return newStr;
 }
